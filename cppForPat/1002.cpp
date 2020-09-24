743edb45c1fe7328f95a8f847009b74792a2174f
Lichen Wu <wulicheneason@gmail.com>
2020-09-24 17:10:19 -0600

    time for zoom

--- cppForPat/1002.cpp
+++ cppForPat/1002.cpp
@@ -9,6 +9,7 @@ int main()
 	string mystr;
 	string rMystr;
 	int i, r, sum = 0;
+	char inNum[100];
 	map<int, string> pinyinMap;
 	pinyinMap[0] = "ling";
 	pinyinMap[1] = "yi";
@@ -22,27 +23,31 @@ int main()
 	pinyinMap[9] = "jiu";
 
 
-	cout << "Enter a number\n";
-	getline(cin, mystr);
-	stringstream(mystr) >> i;
-	while (i > 0)
+	cin.getline(inNum, 100);
+	mystr = string(inNum);	
+	//stringstream(mystr) >> i;
+	for (string::iterator it = mystr.begin(); it != mystr.end(); ++it)
 	{
-		r = i % 10;
-		sum += r;
-		i /= 10;
+		cout << int(*it);
 	}
-	
+	cout << "All digits of your number:" << sum << endl;
+	//while (i > 0)
+	//{
+	//	r = i % 10;
+	//	sum += r;
+	//	i /= 10;
+	//}
 
-	mystr.clear();
-	while (sum > 0)
-	{
-		r = sum % 10;
-		rMystr = pinyinMap[r];
-		mystr = rMystr + " "+ mystr;
-		sum /= 10;
-	}
+	//mystr.clear();
+	//while (sum > 0)
+	//{
+	//	r = sum % 10;
+	//	rMystr = pinyinMap[r];
+	//	mystr = rMystr + " " + mystr;
+	//	sum /= 10;
+	//}
 
-	cout << "The sum of digits is: " << mystr << endl;
+	//cout << mystr << endl;
 
 	return 0;
 

e3ec67720362c17b8a3e3217fa567c1769f9f06a
Lichen Wu <wulicheneason@gmail.com>
2020-09-24 16:07:58 -0600

    simple happiness

--- cppForPat/1002.cpp
+++ cppForPat/1002.cpp
@@ -1,1 +1,21 @@
-#include
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+	string mystr;
+	int i, r, sum = 0;
+	cout << "Enter a number\n";
+	getline(cin, mystr);
+	stringstream(mystr) >> i;
+	while (i > 0)
+	{
+		r = i % 10;
+		sum += r;
+		i /= 10;
+	}
+	cout << "The sum of digits is: " << sum << endl;
+	return 0;
+}

3b3445cddd282555c4408864a09e609219b98b46
EC2 Default User <wulicheneason@gmail.com>
2020-09-25 05:18:13 +0000

    char_traits<char>::length inNum[n] - '0'

--- cppForPat/1002.cpp
+++ cppForPat/1002.cpp
@@ -10,6 +10,7 @@ int main()
 	string rMystr;
 	int i, r, sum = 0;
 	char inNum[100];
+	char * pChar;
 	map<int, string> pinyinMap;
 	pinyinMap[0] = "ling";
 	pinyinMap[1] = "yi";
@@ -24,30 +25,22 @@ int main()
 
 
 	cin.getline(inNum, 100);
-	mystr = string(inNum);	
-	//stringstream(mystr) >> i;
-	for (string::iterator it = mystr.begin(); it != mystr.end(); ++it)
+	
+	for (int n = 0; n < char_traits<char>::length(inNum); n++)
 	{
-		cout << int(*it);
+		r = (int)(inNum[n] - '0');
+		sum += r;
 	}
-	cout << "All digits of your number:" << sum << endl;
-	//while (i > 0)
-	//{
-	//	r = i % 10;
-	//	sum += r;
-	//	i /= 10;
-	//}
 
-	//mystr.clear();
-	//while (sum > 0)
-	//{
-	//	r = sum % 10;
-	//	rMystr = pinyinMap[r];
-	//	mystr = rMystr + " " + mystr;
-	//	sum /= 10;
-	//}
+	while (sum > 0)
+	{
+		r = sum % 10;
+		rMystr = pinyinMap[r];
+		mystr = rMystr + " " + mystr;
+		sum /= 10;
+	}
 
-	//cout << mystr << endl;
+	cout << mystr << endl;
 
 	return 0;
 

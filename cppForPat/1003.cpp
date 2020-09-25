e272adb0ed64c8dba9dccac613208a4ab2d5aeab
Lichen Wu <wulicheneason@gmail.com>
2020-09-25 14:12:24 -0600

    C++ nitty gritty goLang PyTorch
    it does not matter. CONCERNTRACE MORE ON YOUR PHD DEGREE

---
+++ cppForPat/1003.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	char inNum[10];
+	cin.getline(inNum, 1);
+	cout << "from 1003\n";
+
+	return 0;
+
+}

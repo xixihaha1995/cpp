6e2a3e1bcf21dc684a411b51e5215d93765ad0d0
EC2 Default User <wulicheneason@gmail.com>
2020-10-07 04:43:01 +0000

    how to put auto with [] together

---
+++ CS106LTutorial/makePair.cpp
@@ -0,0 +1,12 @@
+#include <utility> //std::pair
+#include <iostream> //std::cout
+
+int main()
+{
+    auto p = std::make_pair("s", 5);
+    // auto a;
+    // auto b;
+    // auto [a, b] = p;
+
+    std::cout << "p: " << p.first << "," << p.second << std::endl;
+}

362260566e1d4433cbd490da940e34a13da79a63
EC2 Default User <wulicheneason@gmail.com>
2020-10-07 04:23:41 +0000

    std namespace

--- CS106L.cpp
+++ CS106L.cpp
@@ -1,16 +1,30 @@
 #include <string>
+#include <sstream>
+#include <iostream>
 
-struct TestStruct {
-    string name;
-    string state;
-    int age;
-}
 
-TestStruct t;
-t.name = "Bob";
-t.state = "WY";
-t.age = 26;
+struct movies_t {
+    std::string title;
+    int year;
+} mine, yours;
 
-void func(double c) {
-    std:: cout << b << :: endl;
+void printmovie (movies_t movie);
+
+int main()
+{
+    std::string mystr;
+    mine.title = "Three Body";
+    mine.year = 2077;
+
+    std::cout << "Enter title\n";
+    std::getline(std::cin, yours.title);
+    std::cout << "Enter year\n";
+    std::getline(std::cin, mystr);
+    std::stringstream(mystr) >> yours.year;
+
+    std::cout << "My movie is: \n";
+    printmovie(mine);
+    std::cout << "And your movie is: \n";
+    printmovie(yours);
+
 }

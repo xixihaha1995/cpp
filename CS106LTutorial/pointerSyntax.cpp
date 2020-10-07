e3ef8512eb17815820214ad00ec95745b3564859
EC2 Default User <wulicheneason@gmail.com>
2020-10-07 06:15:45 +0000

    * is part of point x1's  compound type specifier

--- CS106LTutorial/pointerSyntax.cpp
+++ CS106LTutorial/pointerSyntax.cpp
@@ -2,6 +2,6 @@
 #include <type_traits>
 int main () 
 {
-    int * x1, x2;
-    std::cout<<"x1 is " << typeid(*x1).name() << std::endl;
+    int* x1, x2;
+    std::cout<<"x1 is " << typeid(x2).name() << std::endl;
 }

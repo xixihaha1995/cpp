405b1483a1b3787a3c409f2cc77b4d7502e6bfbe
EC2 Default User <wulicheneason@gmail.com>
2020-10-07 03:57:58 +0000

    struct

---
+++ CS106L.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+struct TestStruct {
+    string name;
+    string state;
+    int age;
+}
+
+TestStruct t;
+t.name = "Bob";
+t.state = "WY";
+t.age = 26;
+
+void func(double c) {
+    std:: cout << b << :: endl;
+}

f5715cfef72359daf59a899f1b7362c4000416ab
EC2 Default User <wulicheneason@gmail.com>
2020-10-07 06:45:36 +0000

    debug for test send reference to function

---
+++ CS106LTutorial/constRef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void test(const int * ref);
+
+int main() 
+{
+    int para = 5;
+    // const int & ref = para;
+    test(para);
+    std::cout << "para is: " << para << std::endl;
+    return 0;
+}
+void test(const int& ref)
+{
+    int a = ref * 2;
+    std::cout<<"a is: " << a << "," << "ref is: " << ref << std::endl;
+}
--- CS106LTutorial/pointerSyntax.cpp
+++ CS106LTutorial/pointerSyntax.cpp
@@ -4,4 +4,5 @@ int main ()
 {
     int* x1, x2;
     std::cout<<"x1 is " << typeid(x2).name() << std::endl;
+    return 0;
 }

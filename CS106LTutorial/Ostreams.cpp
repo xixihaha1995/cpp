56312ba2c4ac3eff8e2242ade94ebb3f823b2548
EC2 Default User <wulicheneason@gmail.com>
2020-10-10 04:45:04 +0000

    streams

---
+++ CS106LTutorial/Ostreams.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+
+
+int main(){
+    int x, z; std::string y; 
+    std::cin >> x >> y >> z;
+    std::cout<< "x is: " << x << "y is: "<< y << "z is: " << z<<std::endl;
+    return 0;
+}

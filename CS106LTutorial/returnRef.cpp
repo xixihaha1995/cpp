c2e7fa661398ddf529b3018d129457c56748f95b
EC2 Default User <wulicheneason@gmail.com>
2020-10-10 04:10:41 +0000

    func recv & ref return &ref and vector<int>::iterator

---
+++ CS106LTutorial/returnRef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int& front(std::vector<int>& vec) {
+    return vec[0];
+}
+
+int main() {
+    std::vector<int> numbers{1,6,6};
+    front(numbers) = 6;
+
+    for (std::vector<int>::iterator it = numbers.begin(); it != numbers.end(); ++it) {
+        std::cout << ' ' << *it;
+    }
+    return 0;
+}

47afeccbd3c259c05ef316f563d1ddd1e737b7b1
EC2 Default User <wulicheneason@gmail.com>
2020-10-12 04:04:28 +0000

    iterator for set is std::Rb_tree_const_iterator<int>

--- CS106LTutorial/itretor.cpp
+++ CS106LTutorial/itretor.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <set>
 
 int main() {
     std::map<int,int> map {{1,2},{6,99}};
@@ -8,5 +9,13 @@ int main() {
 
     std::cout << (*it).first <<std::endl;
     std::cout << map_elem.second << std::endl;
+    
+    std::set<int> set {3, 1, 4, 1, 5, 9};
+    for (auto itSet = set.begin(); itSet != set.end(); ++itSet) {
+        const auto& elem = itSet;
+        std::cout << *elem << std::endl;
+    }
+
+
     return 0;
 }

c2ed69edcac961b73f231d4bc6c2cc2377ea3cd5
EC2 Default User <wulicheneason@gmail.com>
2020-10-12 04:12:26 +0000

    why cannot print all pairs in a map

--- CS106LTutorial/itretor.cpp
+++ CS106LTutorial/itretor.cpp
@@ -3,20 +3,20 @@
 #include <set>
 
 int main() {
-    std::map<int,int> map {{1,2},{6,99}};
-    auto it = map.begin();
-    auto map_elem = *it;
+    // std::map<int,int> map {{1,2},{6,99}};
+    // auto it = map.begin();
+    // auto map_elem = *it;
 
-    std::cout << (*it).first <<std::endl;
-    std::cout << map_elem.second << std::endl;
-    
-    std::set<int> set {3, 1, 4, 1, 5, 9};
-    for (auto itSet = set.begin(); itSet != set.end(); ++itSet) {
-        const auto& elem = itSet;
-        std::cout << *elem << std::endl;
-    }
+    // std::cout << (*it).first <<std::endl;
+    // std::cout << map_elem.second << std::endl;
+    // int key, value;
+    // std::set<int> set {3, 1, 4, 1, 5, 9};
+    // for (auto itSet = set.begin(); itSet != set.end(); ++itSet) {
+    //     const auto& elem = itSet;
+    //     std::cout << *elem << std::endl;
+    // }
 
-    std::map<int> map  {{1, 6}, {1, 8}, {0, 3}, {3, 9}};
+    std::map<int, int> map {{1, 6}, {1, 8}, {0, 3}, {3, 9}};
     for (auto itMap = map.begin(); itMap != map.end(); ++itMap) {
         const auto& [key, value] = *itMap;
         std::cout << key << ":" << value << std::endl;

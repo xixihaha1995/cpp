efdd8b6cef367708bc5710bf5fd2fad924deca9f
EC2 Default User <wulicheneason@gmail.com>
2020-10-14 03:09:07 +0000

    compare string with template

---
+++ CS106LTutorial/template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+template <typename T>
+T my_min(const T& a, const T& b) {
+    return a < b ? a : b;
+}
+
+int main () {
+    std::string c = "nihao";
+    std::string d = "buhao";
+    std::string retur = my_min<std::string>(c, d);
+
+    std::cout << retur << std::endl;
+    return 0;
+}

d934e2a457b0e1a0f5dcb308839cfde381432e61
EC2 Default User <wulicheneason@gmail.com>
2020-10-14 03:10:23 +0000

    got number compare

--- CS106LTutorial/template.cpp
+++ CS106LTutorial/template.cpp
@@ -8,8 +8,11 @@ T my_min(const T& a, const T& b) {
 int main () {
     std::string c = "nihao";
     std::string d = "buhao";
+    
     std::string retur = my_min<std::string>(c, d);
-
+    int number = my_min(2,-6);
     std::cout << retur << std::endl;
+    std::cout << number << std::endl;
+
     return 0;
 }

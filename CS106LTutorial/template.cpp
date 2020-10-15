88124d9b46741f927be9d3d90eb7a9d5d45ee6af
EC2 Default User <wulicheneason@gmail.com>
2020-10-15 02:42:04 +0000

    vector is a member of std

--- CS106LTutorial/template.cpp
+++ CS106LTutorial/template.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
 template <typename T, typename U>
 auto my_min(const T& a, const U& b) {
     return a < b ? a : b;
 }
 
 template <typename T>
-T my2min(vector<T>& nums) {
+T my2min(std::vector<T>& nums) {
     T elem = nums[0];
     if (nums.size() == 1) return elem;
     auto min = my2min(nums.subList(1));
@@ -20,10 +21,10 @@ int main () {
     
     // std::string retur = my_min<std::string>(c, d);
     // auto retur = my_min("neo", "leo");
-
+    auto minVec = my2min(4.2, -7.9, 8.223, 0.0, 1.753);
     // int number = my_min(2.7,-6.2);
     // std::cout << retur[0] << std::endl;
-    std::cout << my_min(2.7,-6.2) << std::endl;
+    std::cout << minVec << std::endl;
 
     return 0;
 }

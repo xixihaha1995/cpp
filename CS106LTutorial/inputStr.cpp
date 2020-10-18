f047c568c9ccc1b4860b63f78ade996108bfebe9
EC2 Default User <wulicheneason@gmail.com>
2020-10-18 03:38:29 +0000

    cin stream is spaced

---
+++ CS106LTutorial/inputStr.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main () {
+    int value;
+    int value2;
+    std::cin >> value >> value2;
+    std::cout << value <<std::endl;
+    std::cout << value2 << std::endl;
+    return 0;
+}

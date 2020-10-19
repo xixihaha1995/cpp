c633a215fa4afc8186919ad0896df1d0a8a2683b
EC2 Default User <wulicheneason@gmail.com>
2020-10-19 04:21:00 +0000

    back inseter is a special case of outpu iterator

--- CS106LTutorial/algo.cpp
+++ CS106LTutorial/algo.cpp
@@ -3,9 +3,9 @@
 
 int main () {
     std::vector<int> v {66,72 };
-    std::vector<int> vCopy(v.size());
+    std::vector<int> vCopy;
 
-    std::copy(v.begin(), v.end(), vCopy.begin());
+    std::copy(v.begin(), v.end(), std::back_inserter(vCopy));
     for (auto elem:vCopy) {
         std::cout << elem << std::endl;
     }

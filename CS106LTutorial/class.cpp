d69b97ddfa218414437033d2fa9d7480cc0a8b74
EC2 Default User <wulicheneason@gmail.com>
2020-10-19 20:59:28 +0000

    constructor(required to call)<-memeber function

--- CS106LTutorial/class.cpp
+++ CS106LTutorial/class.cpp
@@ -4,19 +4,23 @@ class Rectangle {
     int width, height;
 
     public:
+    Rectangle(int, int);
     void set_value(int, int);
     int area() {return width*height;}
 
 };
-
+Rectangle::Rectangle(int a, int b) {
+    width = a;
+    height = b;
+}
 void Rectangle::set_value(int x, int y) {
     width = x;
     height = y;
 }
-
+//scope operator two colons, ::
 // class internal function can be defined out of class
 int main() {
-    Rectangle rect;
+    Rectangle rect(2,7);
     rect.set_value(3,4);
     std::cout << "area: " << rect.area() << std::endl;
 

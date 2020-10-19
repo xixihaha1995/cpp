4bbeeff079c8446a365ff9490e298fe9c7020379
EC2 Default User <wulicheneason@gmail.com>
2020-10-19 21:10:38 +0000

    constructor of a class can be called with many types

--- CS106LTutorial/class.cpp
+++ CS106LTutorial/class.cpp
@@ -4,7 +4,7 @@ class Rectangle {
     int width, height;
 
     public:
-    Rectangle();
+    Rectangle();//but default constructor definition should be defined with ()
     Rectangle(int, int);
     void set_value(int, int);
     int area() {return width*height;}
@@ -25,9 +25,11 @@ void Rectangle::set_value(int x, int y) {
 //scope operator two colons, ::
 // class internal function can be defined out of class
 int main() {
-    Rectangle rect(2,7);
-    std::cout << "area: " << rect.area() << std::endl;
-    Rectangle rect0;
+    Rectangle rect(2,7);//functional form
+    Rectangle assignRect = {5,6};//POD like
+    Rectangle uniIniRect {3,9};
+    std::cout << "area: " << uniIniRect.area() << std::endl;
+    Rectangle rect0;//default constructor cannot be called with (), it takes no parameters
     std::cout << "area: " << rect0.area() << std::endl;
 
     return 0;

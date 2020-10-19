01b707051ad1fb3feb9c46a778a9d241e5e57396
EC2 Default User <wulicheneason@gmail.com>
2020-10-19 20:10:34 +0000

    reference of a variable can be gotten from  function

--- CS106LTutorial/overloading.cpp
+++ CS106LTutorial/overloading.cpp
@@ -4,16 +4,21 @@
 // bool operator==(const Point& lhs, const Point& rhs) {
 //     return lhs.x == rhs.x && lhs.y == rhs.y;
 // }
-
+int global = 1;
+int& getGlobal() {
+    return global;
+}
 int main() {
     // Set& operator+=(const ValueType& value) {
 
     // }
-    std::set<int> numSet;
-    numSet += 66;
-    numSet += 23;
-    for (auto elem: numSet) {
-        std::cout << elem << std::endl;
-    }
+    // std::set<int> numSet;
+    // numSet += 66;
+    // numSet += 23;
+    // for (auto elem: numSet) {
+    //     std::cout << elem << std::endl;
+    // }
+    getGlobal() += 2;
+    std::cout << global << std::endl;
     return 0;
 }
